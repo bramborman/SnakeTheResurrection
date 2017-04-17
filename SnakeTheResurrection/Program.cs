6e7b883544181f2a13bb6bf617a0619dcce06efe
Marian Dolinský <mariandolinsky@outlook.com>
2017-04-17 19:26:04 +0200

    Added blank solution

---
+++ SnakeTheResurrection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeTheResurrection
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Go away!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SnakeTheResurrection/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Snake The Resurrection")]
+[assembly: AssemblyProduct("Snake The Resurrection")]
+[assembly: AssemblyDescription("Snake The Resurrection")]
+[assembly: AssemblyCompany("Marian Dolinský")]
+[assembly: AssemblyCopyright("© 2017 Marian Dolinský")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
