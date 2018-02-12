a01068ec1c4c33d6c0e84d4b4b9a6141ac0d5ab0
Marian Dolinský <mariandolinsky@outlook.com>
2018-02-13 00:06:54 +0100

    Added Panel class

---
+++ SnakeTheResurrection/Utilities/UI/Panel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace SnakeTheResurrection.Utilities.UI
+{
+    public abstract class Panel : ContentElement
+    {
+        public ObservableCollection<UIElement> Items { get; } = new ObservableCollection<UIElement>();
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(); }
+            set { SetValue(value); }
+        }
+
+        public Panel()
+        {
+            RegisterProperty(nameof(Orientation), typeof(Orientation), Orientation.Vertical);
+            Items.CollectionChanged += Items_CollectionChanged;
+        }
+
+        private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Move)
+            {
+                return;
+            }
+
+            foreach (UIElement oldItem in e.OldItems)
+            {
+                if (ReferenceEquals(this, oldItem.Parent))
+                {
+                    SetParent(oldItem, null);
+                }
+            }
+
+            foreach (UIElement newItem in e.NewItems)
+            {
+                SetParent(newItem, this);
+            }
+        }
+    }
+}
--- SnakeTheResurrection/Utilities/UI/StackPanel.cs
+++ SnakeTheResurrection/Utilities/UI/StackPanel.cs
@@ -1,45 +1,9 @@
 ﻿using System;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 
 namespace SnakeTheResurrection.Utilities.UI
 {
-    public class StackPanel : ContentElement
+    public class StackPanel : Panel
     {
-        public ObservableCollection<UIElement> Items { get; } = new ObservableCollection<UIElement>();
-        public Orientation Orientation
-        {
-            get { return (Orientation)GetValue(); }
-            set { SetValue(value); }
-        }
-        
-        public StackPanel()
-        {
-            RegisterProperty(nameof(Orientation), typeof(Orientation), Orientation.Vertical);
-            Items.CollectionChanged += Items_CollectionChanged;
-        }
-
-        private void Items_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Move)
-            {
-                return;
-            }
-
-            foreach (UIElement oldItem in e.OldItems)
-            {
-                if (ReferenceEquals(this, oldItem.Parent))
-                {
-                    SetParent(oldItem, null);
-                }
-            }
-
-            foreach (UIElement newItem in e.NewItems)
-            {
-                SetParent(newItem, this);
-            }
-        }
-
         public override Rectangle Render(in Rectangle bounds)
         {
             Rectangle area = base.Render(in bounds);
